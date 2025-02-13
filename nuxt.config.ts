6dd4214c06c6f200c2098beb170bcd69079f9b3d
Fariz313 <fariz313.akbar@gmail.com>
2025-02-13 22:13:29 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 export default defineNuxtConfig({
   app: {
+    baseURL: "/healthapps/",
+    buildAssetsDir: 'assets',
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
